e0111bcfe9cf5d7d40c4a3779ecb22b061aae2a4
Yevhen_Tkachenko <Yevhen_Tkachenko@epam.com>
2023-07-18 12:25:39 +0300

    update backend: - allow CrossOrigin for UI - refactor Controller - Remove unused Utils

--- src/main/java/com/yevhent/fullstackdemo/controller/ContactsController.java
+++ src/main/java/com/yevhent/fullstackdemo/controller/ContactsController.java
@@ -2,8 +2,8 @@ package com.yevhent.fullstackdemo.controller;
 
 import com.yevhent.fullstackdemo.model.Contact;
 import com.yevhent.fullstackdemo.repository.ContactRepository;
-import com.yevhent.fullstackdemo.util.CollectionUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,32 +12,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Collection;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/contacts")
+@CrossOrigin("http://localhost:3000")
 @RequiredArgsConstructor
-public class ApiController {
+public class ContactsController {
 
     private final ContactRepository contactRepository;
 
-    @GetMapping("/contacts")
-    public List<Contact> getContacts() {
-        return CollectionUtil.toList(contactRepository.findAll());
+    @GetMapping
+    public Collection<Contact> getContacts() {
+        return (Collection<Contact>) contactRepository.findAll();
     }
 
-    @GetMapping("/contacts/{id}")
+    @GetMapping("/{id}")
     public Optional<Contact> getContact(@PathVariable Long id) {
         return contactRepository.findById(id);
     }
 
-    @PostMapping("/contacts")
+    @PostMapping
     public Contact addContact(@RequestBody Contact contact) {
         return contactRepository.save(contact);
     }
 
-    @DeleteMapping("/contacts/{id}")
+    @DeleteMapping("/{id}")
     public void deleteContact(@PathVariable Long id) {
         contactRepository.deleteById(id);
     }
