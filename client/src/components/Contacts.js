8922bbc8a7bf2c8809c837919a137af2bdf2d4d4
Yevhen_Tkachenko <Yevhen_Tkachenko@epam.com>
2023-07-18 12:29:38 +0300

    introduce dynamic frontend: - add component to show Single Contact - add fetch function to get all Contacts from backend - add content to show all fetched Contacts

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SingleContact from './SingleContact'
 
 export default class Contacts extends Component {
     constructor(props){
@@ -8,10 +9,18 @@ export default class Contacts extends Component {
         };
     }
 
+    componentDidMount(){
+        fetch('http://localhost:8083/api/contacts')
+        .then(response => response.json())
+        .then(data => this.setState({contacts: data}))
+    }
+
     render() {
         const content = (
-            <div>
-                <p>Hello Yevhen</p>
+            <div className="row">
+                { this.state.contacts.map((item) => (
+                    <SingleContact key={item.id} item={item} />
+                ))}
             </div>
         )
         return content;
